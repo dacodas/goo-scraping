8fbf4a6dc8f7f959143b13ada09a38c2e0c36b8f
Dacoda Strack <dacoda.strack@gmail.com>
2018-11-09 11:10:58 -0800

    figuring out regex matching for readings

--- goo-scraping.lisp
+++ goo-scraping.lisp
@@ -8,6 +8,10 @@
 
 (in-package :goo-scraping)
 
+(defparameter *entries-directory* (make-pathname :directory (append
+                                                     (pathname-directory (asdf:system-source-directory :goo-scraping))
+                                                     '("dictionary-entries"))))
+
 (defparameter *failures* 0)
 (defparameter *user-agents*  (with-open-file (user-agents-stream "/home/dacoda/quicklisp/local-projects/goo-scraping/user-agents.txt")
                                  (loop for x = (read-line user-agents-stream nil nil)
@@ -15,24 +19,23 @@
 (defparameter *number-of-worker-threads* (floor (* 1.25 (length *user-agents*))))
 (setf lparallel:*kernel* (lparallel:make-kernel *number-of-worker-threads*))
 
-(defun grab-goo-pages (start number-of-pages)
-  (let ((futures (make-array number-of-pages)))
+(defun grab-goo-pages (entries-array) 
+  (let* ((number-of-pages (length entries-array))
+         (futures (make-array number-of-pages)))
     (loop for i below number-of-pages
        do (let* ((user-agent-id (random (length *user-agents*)))
                  (user-agent (first (nth user-agent-id *user-agents*))))
             (setf (aref futures i)
                   (eval `(lparallel:future
                            (values-list (append (multiple-value-list
-                                                 (drakma:http-request (format nil "https://dictionary.goo.ne.jp/jn/~A/meaning/m0u/" (+ ,start ,i))
+                                                 (drakma:http-request (format nil "https://dictionary.goo.ne.jp/jn/~A/meaning/m0u/" ,(aref entries-array i))
                                                                       :user-agent ,user-agent 
                                                                       :cookie-jar (second (nth ,user-agent-id *user-agents*))))
                                                 (list ,user-agent))))))))
     futures))
 
 (defun save-goo-pages (promises)
-  (let ((output-directory (make-pathname :directory (append
-                                                     (pathname-directory (asdf:system-source-directory :goo-scraping))
-                                                     '("dictionary-entries")))))
+  (let ((output-directory *entries-directory*))
     (ensure-directories-exist output-directory)
     (loop for promise across promises
        collect (let* ((response (multiple-value-list (lparallel:force promise)))
@@ -79,14 +82,6 @@
             (let ((page-promises (grab-goo-pages current-start-value number-per-chunk)))
               (save-goo-pages page-promises))))))
 
-#+nil(let ((entries-left (length *missing-entries*))
-      (number-per-chunk 100)
-      (number-of-chunks (floor entries-left number-per-chunk)))
-
-  (dotimes (chunk number-of-chunks)
-
-    (let ((promises (make-array number-per-chunk))))))
-
 (defun find-missing-entries ()
   (defparameter *missing-entries*
     (let* ((number-of-entries (parse-integer
@@ -120,3 +115,45 @@
                 (incf counter)))
         (format t "Done~%")
         missing-entries))))
+
+(defun grab-missing-pages ()
+  (find-missing-entries)
+  (let* ((entries-left (length *missing-entries*))
+         (number-per-chunk 5)
+         (number-of-chunks (floor entries-left number-per-chunk)))
+    (dotimes (chunk number-of-chunks)
+      (sleep (random (* 2 0.5)))
+      (save-goo-pages
+       (grab-goo-pages
+        (make-array number-per-chunk :displaced-to *missing-entries* :displaced-index-offset (* number-per-chunk chunk)))))))
+
+(defun simplify-entry (entry-number)
+  (let* ((simplified-entries-directory (make-pathname :directory (append
+                                                                  (pathname-directory (asdf:system-source-directory :goo-scraping))
+                                                                  '("simplified-dictionary-entries"))))
+         (simplified-entry (merge-pathnames (make-pathname :name (format nil "~A" entry-number) :type "html")
+                                            simplified-entries-directory))
+         (original-entry (merge-pathnames (make-pathname :directory (pathname-directory *entries-directory*))
+                                          simplified-entry)))
+    (ensure-directories-exist simplified-entries-directory)
+    (with-open-file (output-file simplified-entry
+                                 :direction :output
+                                 :if-exists :supersede
+                                 :if-does-not-exist :create)
+      (let ((document (lquery:$1 (initialize original-entry))))
+        (format output-file "~A~%~A"
+                (lquery:$1 document "div.basic_title h1" (text-without-comments))
+                (lquery:$1 document "div.meaning_area" (serialize)))))))
+
+(let* ((start 2)
+       (end 259849)
+       (number-per-chunk 500)
+       (number-of-chunks (ceiling (1+ (- end start)) number-per-chunk))
+       (chunk (make-array number-per-chunk)))
+  (dotimes (chunk-number number-of-chunks)
+    (dotimes (current-iteration number-per-chunk)
+      (let ((current-entry (+ (* number-per-chunk chunk-number) current-iteration start)))
+        (setf (aref chunk current-iteration)
+              (eval `(lparallel:future (simplify-entry ,current-entry))))))
+    (loop for entry across chunk do (lparallel:force entry))))
+
